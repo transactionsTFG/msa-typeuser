1537c7982d7f72cd6a7fd443a0e000da3c6f17b1
josevillacres46 <jvillacr@ucm.es>
2025-03-31 18:44:45 +0200

    FEAT: Pom TypeUser

---
+++ src/main/java/business/typeuser/TypeUser.java
@@ -0,0 +1,23 @@
+package business.typeuser;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class TypeUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @Version
+    private int version;
+}
